3e9a44364fec25ded54a9d4be642586400b4e3ed
madhundle <madeline.hundley@gmail.com>
2024-05-01 14:57:08 -0500

    Basic component structure added with routing

---
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-past-trainings',
+  templateUrl: './past-trainings.component.html',
+  styleUrl: './past-trainings.component.scss'
+})
+export class PastTrainingsComponent {
+
+}
